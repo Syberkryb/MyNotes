58869b6f53fcc9cf92d5b00cc1416ee1cfaac8a5
Victor Süberkrüb <victorsubskole@gmail.com>
2023-08-31 12:27:09 +0200

    Start

--- Program.cs
+++ Program.cs
@@ -1,1 +1,25 @@
-﻿
+﻿List<Page>pages = new List<Page>();
+
+Page side1 = new Page(MyNotes.VariablesPageOne);
+Page side2 = new Page(MyNotes.VariablesPageTwo);
+
+pages.Add(side1);
+pages.Add(side2);
+
+
+Chapter kap1 = new Chapter("Var");
+kap1.Pages = pages;
+Chapter kap2 = new Chapter("Var2", pages);
+
+kap1.Read();
+kap2.Read();
+
+List<Chapter> chapters = new List<Chapter>();
+chapters.Add(kap1);
+chapters.Add(kap2);
+
+Book mybook = new Book("Programming notes",chapters);
+
+
+//pause
+Console.ReadLine();
