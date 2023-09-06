2b2f0e8918996fd63b90f94cf876d4c6ae8e4d83
Victor Süberkrüb <victorsubskole@gmail.com>
2023-09-06 11:06:59 +0200

    Mere tekst på Int og Var

--- Program.cs
+++ Program.cs
@@ -1,18 +1,23 @@
 ﻿List<Page>VarPages = new List<Page>();
 
 Page side1 = new Page(MyNotes.Variables);
+
+VarPages.Add(side1);
+
+List<Page>IntPages = new List<Page>();
+
 Page side2 = new Page(MyNotes.integer1);
 Page side3 = new Page(MyNotes.integer2);
 
-VarPages.Add(side1);
+IntPages.Add(side2); IntPages.Add(side3);
 
 
 
 Chapter kap1 = new Chapter("Variables");
 kap1.Pages = VarPages;
 
-Chapter kap2 = new Chapter("Var2");
-
+Chapter kap2 = new Chapter("Integers");
+kap2.Pages = IntPages;
 
 List<Chapter> chapters = new List<Chapter>();
 chapters.Add(kap1);
