928a83afda781c52c020fb2f9749e21afe1e25ba
Victor Süberkrüb <victorsubskole@gmail.com>
2023-09-06 12:10:56 +0200

    Float sider tilføjet

--- MyNotes.cs
+++ MyNotes.cs
@@ -6,7 +6,7 @@ In C#, variables are fundamental elements used for storing and manipulating data
 They serve as placeholders to store various types of values, such as numbers, text, and objects. 
 Understanding how to declare, initialize, and use variables is crucial for writing effective C# code.
 ";
-
+//(((((((((((((((((((((((((((((((((())))))))))))))))))))))))))))))))))
     public static string integer1 =
 @"...
 In C#, integers are a fundamental data type used to represent whole numbers. 
@@ -16,7 +16,6 @@ Here's an overview of integers in C#:
 
  public static string integer2 =
 @"...
-
 1.	Data Range: Integers in C# can represent a wide range of whole numbers, both positive and negative. 
     The specific range and size of integers depend on the data type chosen. 
     Common integer data types in C# include int, long, short, and byte. 
@@ -32,7 +31,6 @@ Here's an overview of integers in C#:
 
  public static string integer3 =
 @"...
-
 4.	Arithmetic Operations: You can perform various arithmetic operations on integer values, including addition, subtraction, multiplication, and division. 
     Integer division (using the / operator) results in a truncated integer value, discarding the fractional part.
 
@@ -63,4 +61,55 @@ Here's an overview of integers in C#:
 In summary, integers are essential data types in C# for working with whole numbers, providing a wide range of options to suit different requirements. 
 Understanding their characteristics, limitations, and appropriate use cases is crucial for effective C# programming.
 ";
+//(((((((((((((((((((((((((((((((((((((((())))))))))))))))))))))))))))))))))))))))
+public static string float1 =
+@"...
+In C#, floating-point numbers are used to represent real numbers with decimal places.
+They are part of the primitive data types and are particularly useful for tasks involving fractional values or values with a wide range. 
+Here's an overview of floating-point numbers (floats) in C#:
+
+";
+public static string float2 =
+@"...
+1.	Data Range: Floats in C# are 32-bit floating-point numbers that conform to the IEEE 754 standard.
+    They can represent a wide range of values, including both very small and very large numbers. 
+    The range of values they can represent is approximately ±1.5 x 10^-45 to ±3.4 x 10^38.
+
+2.	Precision: Floats have limited precision due to their fixed number of bits. 
+    This means that they might not be able to represent some numbers exactly, especially when dealing with very large or very small values. 
+    Precision issues can lead to rounding errors in calculations.
+
+3.	Default Value: When you declare a float variable without initializing it explicitly, it is assigned a default value of 0.0f.
+
+";
+public static string float3 =
+@"...
+4.	Arithmetic Operations: You can perform standard arithmetic operations on float values, including addition, subtraction, multiplication, and division. 
+    Floats are suitable for tasks that require accurate decimal calculations, such as scientific and financial applications.
+
+5.	Type Conversion: Floats can be converted to other numeric types, including integers, using explicit casting. 
+    However, keep in mind that converting from a float to an integer may result in truncation and loss of data.
+
+6.	Special Values: Floats can represent special values such as positive and negative infinity (float.PositiveInfinity and float.NegativeInfinity) and NaN (Not-a-Number), 
+    which is used to represent undefined or unrepresentable values.
+
+";
+public static string float4 =
+@"...
+7.	Use Cases: Floats are commonly used for tasks involving measurements, scientific computations, graphics, and simulations where precision with decimal places is necessary. 
+    They are also suitable for representing values that can be very large or very small.
+
+8.	Performance: Floating-point operations may be slower than integer operations due to the additional complexity of handling decimal values and rounding. 
+    However, modern processors have dedicated hardware for floating-point calculations, which can optimize performance.
+
+9.	Decimal vs. Float: C# also provides a decimal data type that offers higher precision for decimal calculations but uses more memory than floats. 
+    When working with financial data or scenarios requiring exact decimal calculations, decimal is often preferred over float.
+";
+public static string float5 =
+@"...
+In summary, floats are essential data types in C# for handling real numbers with decimal places, providing a wide range and reasonable precision. 
+They are suitable for a variety of scientific and engineering applications but require attention to precision and potential rounding errors when used in critical calculations.
+
+";
+
 }
--- Program.cs
+++ Program.cs
@@ -4,6 +4,8 @@ Page side1 = new Page(MyNotes.Variables);
 
 VarPages.Add(side1);
 
+
+
 List<Page>IntPages = new List<Page>();
 
 Page side2 = new Page(MyNotes.integer1);
@@ -16,19 +18,38 @@ IntPages.Add(side2); IntPages.Add(side3);
 IntPages.Add(side4); IntPages.Add(side5);
 IntPages.Add(side6);
 
+
+List<Page>FloatPages = new List<Page>();
+
+Page side7 = new Page(MyNotes.float1);
+Page side8 = new Page(MyNotes.float2);
+Page side9 = new Page(MyNotes.float3);
+Page side10 = new Page(MyNotes.float4);
+Page side11 = new Page(MyNotes.float5);
+
+FloatPages.Add(side2); FloatPages.Add(side3); 
+FloatPages.Add(side4); FloatPages.Add(side5);
+FloatPages.Add(side6);
+
+
+
 Chapter kap1 = new Chapter("Variables");
 kap1.Pages = VarPages;
 
-Chapter kap2 = new Chapter("Integers");
+Chapter kap2 = new Chapter("Integer");
 kap2.Pages = IntPages;
+
+Chapter kap3 = new Chapter("Float");
+kap3.Pages = FloatPages;
 
 List<Chapter> chapters = new List<Chapter>();
 chapters.Add(kap1);
 chapters.Add(kap2);
+chapters.Add(kap3);
 
 Book mybook = new Book("Programming notes",chapters);
 
-
+Console.ResetColor();
 mybook.Start();       
     
 
